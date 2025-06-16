9fe573436e9ce8f2e2ac3d231d47912c7a492a57
sepehr <kashanisepehr1992@gmail.com>
2025-06-16 15:10:36 +1000

    Update package

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,26 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import type { ApiResponse } from 'shared/dist'
+import { Hono } from "hono"
+import { cors } from "hono/cors"
+import type { ApiResponse } from "shared/dist"
 
 const app = new Hono()
 
 app.use(cors())
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get("/", (c) => {
+  return c.text("Hello Hono!")
 })
 
-app.get('/hello', async (c) => {
-
+app.get("/hello", async (c) => {
   const data: ApiResponse = {
     message: "Hello BHVR!",
-    success: true
+    success: true,
   }
 
   return c.json(data, { status: 200 })
 })
 
-export default app
+export default {
+  port: Number(process.env.PORT) || 3000,
+  hostname: "0.0.0.0",
+  fetch: app.fetch,
+}
